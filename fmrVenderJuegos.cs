0c9bd384ba9600789deeb0ff53a2bdfa978195f8
José Ricardo García Valladares <GV23010@ti.ues.edu.sv>
2023-10-30 12:18:40 -0600

    se ha codificado el boton registrar compra de juegos

--- fmrVenderJuegos.cs
+++ fmrVenderJuegos.cs
@@ -31,7 +31,45 @@ namespace Clave2_Grupo
 
         private void btnRegistrarCompraJuego_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("SE VENDIO UN PRODUCTO JUEGO");
+            string productoVendido;
+            
+            if (radJuegMecanic.Checked)
+            {
+                productoVendido = radJuegMecanic.Text;
+            }
+            else
+            {
+                if (radJuegElectronic.Checked)
+                {
+                    productoVendido = radJuegElectronic.Text;
+                }
+                else
+                {
+                    productoVendido= "Por favor seleccione un tipo de juego para la compra";
+                    MessageBox.Show(productoVendido, "No seleccionó ningún tipo de juego",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                    return;
+                }
+
+            }
+
+            
+            DateTime fechaActual = DateTime.Now;
+            string mensaje = $"Tipo de Juego: {productoVendido}" + "\n" +
+                            "Fecha y hora actual: " + fechaActual.ToString() + "\n" +
+                            "Día de la semana: " + fechaActual.ToString("dddd");
+
+            string diaSeman = fechaActual.ToString("dddd");
+            MessageBox.Show(mensaje+""+"\nES DIA: "+diaSeman, "SE VENDIÓ UN PRODUCTO JUEGO");
+            ResetearValores();
+        }
+
+        private void ResetearValores()
+        {
+            radJuegMecanic.Checked = false;
+            radJuegElectronic.Checked = false;
+            
+            // Restablecer otros valores si es necesario
+            // ...
         }
     }
 }
