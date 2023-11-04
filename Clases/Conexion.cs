b80b9564e642f0543726d18b3b641e68ba6556c1
Andrea Michelle Rodr�guez Hern�ndez <RH21053@ti.ues.edu.sv>
2023-11-03 22:34:40 -0600

    .

--- Clases/Conexion.cs
+++ Clases/Conexion.cs
@@ -1,14 +1,14 @@
-﻿using MySql.Data.MySqlClient;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Clave2_Grupo.Clases
 {
-    internal class Conexion
+    class Conexion
     {
         MySqlConnection conexion = new MySqlConnection();
 
