8d36dfa14f88e6bf6cdcc5b85760ccbf546b9a81
Andrea Michelle Rodr�guez Hern�ndez <RH21053@ti.ues.edu.sv>
2023-11-06 15:46:00 -0600

    Modifique el nombre de la B.D en la clase conexion

--- Clases/Conexion.cs
+++ Clases/Conexion.cs
@@ -13,7 +13,7 @@ namespace Clave2_Grupo.Clases
         MySqlConnection conexion = new MySqlConnection();
 
         static string servidor = "localhost";
-        static string bd = "clave2_grupo3";
+        static string bd = "clave2_grupo3_";
         static string usuario = "root";
         static string password = "root";
         static string puerto = "3306";
