c948e3cb9758abb49d778c0269df4512c2780365
Andrea Michelle Rodr�guez Hern�ndez <RH21053@ti.ues.edu.sv>
2023-11-07 15:37:49 -0600

    se elimino un metodo de la clase RegistrarCliente

--- Clases/RegistroCliente.cs
+++ Clases/RegistroCliente.cs
@@ -56,27 +56,8 @@ namespace Clave2_Grupo.Clases
             }
         }
 
-            //insertar datos del forms VenderTarjetas a la B.D
+            
 
-            public void guardarTarjetas(ComboBox Tarjeta)
-            {
-                try
-                {
-                    Conexion objetoConexion = new Conexion();
-
-                    String query = "insert into tarjeta(Tipo)" +
-                    "values ('" + Tarjeta.Text+ "');";
-                MySqlCommand myComand = new MySqlCommand(query, objetoConexion.establecerConexion());
-                MySqlDataReader reader = myComand.ExecuteReader();
-                MessageBox.Show("Se registro el tipo de tarjeta seleccionada");
-
-                objetoConexion.cerrarConexion();
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("No se registro el tipo de tarjeta a la base de datos, error: " + ex.ToString());
-                }
-
-            }
+            
     }
 }
