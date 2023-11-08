93e1c0d81a1b37990d0c606be0803619e63c1227
José Ricardo García Valladares <GV23010@ti.ues.edu.sv>
2023-11-08 09:15:10 -0600

    Revert "boton agregado"
    This reverts commit 5cfb4af0ac767993f666cc2f1df0d88fbe066c81.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,7 +30,6 @@ namespace Clave2_Grupo
         private void InitializeComponent()
         {
             this.panelMenuMayorLateral = new System.Windows.Forms.Panel();
-            this.btnConexion = new System.Windows.Forms.Button();
             this.panelSubMenuVentas = new System.Windows.Forms.Panel();
             this.btnVenderJuegos = new System.Windows.Forms.Button();
             this.btnVenderTarjetas = new System.Windows.Forms.Button();
@@ -44,7 +43,7 @@ namespace Clave2_Grupo
             this.panelAbajo = new System.Windows.Forms.Panel();
             this.btnSalir1 = new System.Windows.Forms.Button();
             this.panelContenedorDeFormularios = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnConexion = new System.Windows.Forms.Button();
             this.panelMenuMayorLateral.SuspendLayout();
             this.panelSubMenuVentas.SuspendLayout();
             this.panelSubMenuClientes.SuspendLayout();
@@ -64,20 +63,10 @@ namespace Clave2_Grupo
             this.panelMenuMayorLateral.Controls.Add(this.panelLogo);
             this.panelMenuMayorLateral.Dock = System.Windows.Forms.DockStyle.Left;
             this.panelMenuMayorLateral.Location = new System.Drawing.Point(0, 0);
+            this.panelMenuMayorLateral.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panelMenuMayorLateral.Name = "panelMenuMayorLateral";
-            this.panelMenuMayorLateral.Size = new System.Drawing.Size(250, 518);
+            this.panelMenuMayorLateral.Size = new System.Drawing.Size(333, 638);
             this.panelMenuMayorLateral.TabIndex = 0;
-            // 
-            // btnConexion
-            // 
-            this.btnConexion.Location = new System.Drawing.Point(53, 463);
-            this.btnConexion.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.btnConexion.Name = "btnConexion";
-            this.btnConexion.Size = new System.Drawing.Size(131, 19);
-            this.btnConexion.TabIndex = 7;
-            this.btnConexion.Text = "Comprobar Conexion";
-            this.btnConexion.UseVisualStyleBackColor = true;
-            this.btnConexion.Click += new System.EventHandler(this.btnConexion_Click);
             // 
             // panelSubMenuVentas
             // 
@@ -85,19 +74,21 @@ namespace Clave2_Grupo
             this.panelSubMenuVentas.Controls.Add(this.btnVenderJuegos);
             this.panelSubMenuVentas.Controls.Add(this.btnVenderTarjetas);
             this.panelSubMenuVentas.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panelSubMenuVentas.Location = new System.Drawing.Point(0, 294);
+            this.panelSubMenuVentas.Location = new System.Drawing.Point(0, 361);
+            this.panelSubMenuVentas.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panelSubMenuVentas.Name = "panelSubMenuVentas";
-            this.panelSubMenuVentas.Size = new System.Drawing.Size(250, 125);
+            this.panelSubMenuVentas.Size = new System.Drawing.Size(333, 154);
             this.panelSubMenuVentas.TabIndex = 6;
             // 
             // btnVenderJuegos
             // 
             this.btnVenderJuegos.Dock = System.Windows.Forms.DockStyle.Top;
             this.btnVenderJuegos.FlatAppearance.BorderSize = 0;
-            this.btnVenderJuegos.Location = new System.Drawing.Point(0, 35);
+            this.btnVenderJuegos.Location = new System.Drawing.Point(0, 43);
+            this.btnVenderJuegos.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnVenderJuegos.Name = "btnVenderJuegos";
-            this.btnVenderJuegos.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
-            this.btnVenderJuegos.Size = new System.Drawing.Size(250, 35);
+            this.btnVenderJuegos.Padding = new System.Windows.Forms.Padding(47, 0, 0, 0);
+            this.btnVenderJuegos.Size = new System.Drawing.Size(333, 43);
             this.btnVenderJuegos.TabIndex = 2;
             this.btnVenderJuegos.Text = "VENTA DE JUEGOS";
             this.btnVenderJuegos.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -109,9 +100,10 @@ namespace Clave2_Grupo
             this.btnVenderTarjetas.Dock = System.Windows.Forms.DockStyle.Top;
             this.btnVenderTarjetas.FlatAppearance.BorderSize = 0;
             this.btnVenderTarjetas.Location = new System.Drawing.Point(0, 0);
+            this.btnVenderTarjetas.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnVenderTarjetas.Name = "btnVenderTarjetas";
-            this.btnVenderTarjetas.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
-            this.btnVenderTarjetas.Size = new System.Drawing.Size(250, 35);
+            this.btnVenderTarjetas.Padding = new System.Windows.Forms.Padding(47, 0, 0, 0);
+            this.btnVenderTarjetas.Size = new System.Drawing.Size(333, 43);
             this.btnVenderTarjetas.TabIndex = 1;
             this.btnVenderTarjetas.Text = "VENTA DE TARJETAS";
             this.btnVenderTarjetas.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -126,9 +118,10 @@ namespace Clave2_Grupo
             this.btnMenuVentasAbrir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnMenuVentasAbrir.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnMenuVentasAbrir.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btnMenuVentasAbrir.Location = new System.Drawing.Point(0, 256);
+            this.btnMenuVentasAbrir.Location = new System.Drawing.Point(0, 314);
+            this.btnMenuVentasAbrir.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnMenuVentasAbrir.Name = "btnMenuVentasAbrir";
-            this.btnMenuVentasAbrir.Size = new System.Drawing.Size(250, 38);
+            this.btnMenuVentasAbrir.Size = new System.Drawing.Size(333, 47);
             this.btnMenuVentasAbrir.TabIndex = 5;
             this.btnMenuVentasAbrir.Text = "MENU VENDER (TARJETAS Y JUEGOS)";
             this.btnMenuVentasAbrir.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -139,9 +132,10 @@ namespace Clave2_Grupo
             // 
             this.panelSubMenuTarjetas.BackColor = System.Drawing.Color.Teal;
             this.panelSubMenuTarjetas.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panelSubMenuTarjetas.Location = new System.Drawing.Point(0, 126);
+            this.panelSubMenuTarjetas.Location = new System.Drawing.Point(0, 154);
+            this.panelSubMenuTarjetas.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panelSubMenuTarjetas.Name = "panelSubMenuTarjetas";
-            this.panelSubMenuTarjetas.Size = new System.Drawing.Size(250, 130);
+            this.panelSubMenuTarjetas.Size = new System.Drawing.Size(333, 160);
             this.panelSubMenuTarjetas.TabIndex = 3;
             // 
             // panelSubMenuClientes
@@ -150,19 +144,21 @@ namespace Clave2_Grupo
             this.panelSubMenuClientes.Controls.Add(this.btnConsultarInfoClientes);
             this.panelSubMenuClientes.Controls.Add(this.btnNuevoCliente);
             this.panelSubMenuClientes.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panelSubMenuClientes.Location = new System.Drawing.Point(0, 55);
+            this.panelSubMenuClientes.Location = new System.Drawing.Point(0, 67);
+            this.panelSubMenuClientes.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panelSubMenuClientes.Name = "panelSubMenuClientes";
-            this.panelSubMenuClientes.Size = new System.Drawing.Size(250, 71);
+            this.panelSubMenuClientes.Size = new System.Drawing.Size(333, 87);
             this.panelSubMenuClientes.TabIndex = 1;
             // 
             // btnConsultarInfoClientes
             // 
             this.btnConsultarInfoClientes.Dock = System.Windows.Forms.DockStyle.Top;
             this.btnConsultarInfoClientes.FlatAppearance.BorderSize = 0;
-            this.btnConsultarInfoClientes.Location = new System.Drawing.Point(0, 35);
+            this.btnConsultarInfoClientes.Location = new System.Drawing.Point(0, 43);
+            this.btnConsultarInfoClientes.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnConsultarInfoClientes.Name = "btnConsultarInfoClientes";
-            this.btnConsultarInfoClientes.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
-            this.btnConsultarInfoClientes.Size = new System.Drawing.Size(250, 34);
+            this.btnConsultarInfoClientes.Padding = new System.Windows.Forms.Padding(47, 0, 0, 0);
+            this.btnConsultarInfoClientes.Size = new System.Drawing.Size(333, 42);
             this.btnConsultarInfoClientes.TabIndex = 2;
             this.btnConsultarInfoClientes.Text = "VER INFORMACION DE CLIENTES";
             this.btnConsultarInfoClientes.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -174,9 +170,10 @@ namespace Clave2_Grupo
             this.btnNuevoCliente.Dock = System.Windows.Forms.DockStyle.Top;
             this.btnNuevoCliente.FlatAppearance.BorderSize = 0;
             this.btnNuevoCliente.Location = new System.Drawing.Point(0, 0);
+            this.btnNuevoCliente.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnNuevoCliente.Name = "btnNuevoCliente";
-            this.btnNuevoCliente.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
-            this.btnNuevoCliente.Size = new System.Drawing.Size(250, 35);
+            this.btnNuevoCliente.Padding = new System.Windows.Forms.Padding(47, 0, 0, 0);
+            this.btnNuevoCliente.Size = new System.Drawing.Size(333, 43);
             this.btnNuevoCliente.TabIndex = 1;
             this.btnNuevoCliente.Text = "NUEVO CLIENTE";
             this.btnNuevoCliente.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -190,9 +187,10 @@ namespace Clave2_Grupo
             this.btnMenuClientesAbrir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnMenuClientesAbrir.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnMenuClientesAbrir.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btnMenuClientesAbrir.Location = new System.Drawing.Point(0, 10);
+            this.btnMenuClientesAbrir.Location = new System.Drawing.Point(0, 12);
+            this.btnMenuClientesAbrir.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnMenuClientesAbrir.Name = "btnMenuClientesAbrir";
-            this.btnMenuClientesAbrir.Size = new System.Drawing.Size(250, 45);
+            this.btnMenuClientesAbrir.Size = new System.Drawing.Size(333, 55);
             this.btnMenuClientesAbrir.TabIndex = 0;
             this.btnMenuClientesAbrir.Text = "MENU CLIENTES";
             this.btnMenuClientesAbrir.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -203,26 +201,28 @@ namespace Clave2_Grupo
             // 
             this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelLogo.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panelLogo.Name = "panelLogo";
-            this.panelLogo.Size = new System.Drawing.Size(250, 10);
+            this.panelLogo.Size = new System.Drawing.Size(333, 12);
             this.panelLogo.TabIndex = 0;
             // 
             // panelAbajo
             // 
             this.panelAbajo.BackColor = System.Drawing.Color.Teal;
-            this.panelAbajo.Controls.Add(this.button1);
             this.panelAbajo.Controls.Add(this.btnSalir1);
             this.panelAbajo.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panelAbajo.Location = new System.Drawing.Point(250, 418);
+            this.panelAbajo.Location = new System.Drawing.Point(333, 515);
+            this.panelAbajo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panelAbajo.Name = "panelAbajo";
-            this.panelAbajo.Size = new System.Drawing.Size(778, 100);
+            this.panelAbajo.Size = new System.Drawing.Size(1080, 123);
             this.panelAbajo.TabIndex = 1;
             // 
             // btnSalir1
             // 
-            this.btnSalir1.Location = new System.Drawing.Point(560, 74);
+            this.btnSalir1.Location = new System.Drawing.Point(747, 91);
+            this.btnSalir1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnSalir1.Name = "btnSalir1";
-            this.btnSalir1.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir1.Size = new System.Drawing.Size(100, 28);
             this.btnSalir1.TabIndex = 2;
             this.btnSalir1.Text = "SALIR";
             this.btnSalir1.UseVisualStyleBackColor = true;
@@ -232,28 +232,31 @@ namespace Clave2_Grupo
             // 
             this.panelContenedorDeFormularios.BackColor = System.Drawing.SystemColors.ControlDark;
             this.panelContenedorDeFormularios.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelContenedorDeFormularios.Location = new System.Drawing.Point(250, 0);
+            this.panelContenedorDeFormularios.Location = new System.Drawing.Point(333, 0);
+            this.panelContenedorDeFormularios.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panelContenedorDeFormularios.Name = "panelContenedorDeFormularios";
-            this.panelContenedorDeFormularios.Size = new System.Drawing.Size(778, 418);
+            this.panelContenedorDeFormularios.Size = new System.Drawing.Size(1080, 515);
             this.panelContenedorDeFormularios.TabIndex = 2;
             // 
-            // button1
+            // btnConexion
             // 
-            this.button1.Location = new System.Drawing.Point(82, 45);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(437, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnConexion.Location = new System.Drawing.Point(71, 570);
+            this.btnConexion.Name = "btnConexion";
+            this.btnConexion.Size = new System.Drawing.Size(175, 23);
+            this.btnConexion.TabIndex = 7;
+            this.btnConexion.Text = "Comprobar Conexion";
+            this.btnConexion.UseVisualStyleBackColor = true;
+            this.btnConexion.Click += new System.EventHandler(this.btnConexion_Click);
             // 
             // fmrPrincipal
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1028, 518);
+            this.ClientSize = new System.Drawing.Size(1413, 638);
             this.Controls.Add(this.panelContenedorDeFormularios);
             this.Controls.Add(this.panelAbajo);
             this.Controls.Add(this.panelMenuMayorLateral);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "fmrPrincipal";
             this.Text = ":::GameSIVAR:::";
             this.panelMenuMayorLateral.ResumeLayout(false);
@@ -281,7 +284,6 @@ namespace Clave2_Grupo
         private System.Windows.Forms.Button btnVenderJuegos;
         private System.Windows.Forms.Button btnVenderTarjetas;
         private System.Windows.Forms.Button btnConexion;
-        private System.Windows.Forms.Button button1;
     }
 }
 
