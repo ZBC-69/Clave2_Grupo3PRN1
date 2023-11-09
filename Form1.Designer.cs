7061adb51327405aeb107ec06470fa1112554af4
Andrea Michelle Rodr�guez Hern�ndez <RH21053@ti.ues.edu.sv>
2023-11-09 14:38:29 -0600

    Cambio de icono

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,7 +29,9 @@ namespace Clave2_Grupo
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fmrPrincipal));
             this.panelMenuMayorLateral = new System.Windows.Forms.Panel();
+            this.btnConexion = new System.Windows.Forms.Button();
             this.panelSubMenuVentas = new System.Windows.Forms.Panel();
             this.btnVenderJuegos = new System.Windows.Forms.Button();
             this.btnVenderTarjetas = new System.Windows.Forms.Button();
@@ -43,7 +45,6 @@ namespace Clave2_Grupo
             this.panelAbajo = new System.Windows.Forms.Panel();
             this.btnSalir1 = new System.Windows.Forms.Button();
             this.panelContenedorDeFormularios = new System.Windows.Forms.Panel();
-            this.btnConexion = new System.Windows.Forms.Button();
             this.panelMenuMayorLateral.SuspendLayout();
             this.panelSubMenuVentas.SuspendLayout();
             this.panelSubMenuClientes.SuspendLayout();
@@ -63,10 +64,20 @@ namespace Clave2_Grupo
             this.panelMenuMayorLateral.Controls.Add(this.panelLogo);
             this.panelMenuMayorLateral.Dock = System.Windows.Forms.DockStyle.Left;
             this.panelMenuMayorLateral.Location = new System.Drawing.Point(0, 0);
-            this.panelMenuMayorLateral.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panelMenuMayorLateral.Margin = new System.Windows.Forms.Padding(4);
             this.panelMenuMayorLateral.Name = "panelMenuMayorLateral";
             this.panelMenuMayorLateral.Size = new System.Drawing.Size(333, 638);
             this.panelMenuMayorLateral.TabIndex = 0;
+            // 
+            // btnConexion
+            // 
+            this.btnConexion.Location = new System.Drawing.Point(71, 570);
+            this.btnConexion.Name = "btnConexion";
+            this.btnConexion.Size = new System.Drawing.Size(175, 23);
+            this.btnConexion.TabIndex = 7;
+            this.btnConexion.Text = "Comprobar Conexion";
+            this.btnConexion.UseVisualStyleBackColor = true;
+            this.btnConexion.Click += new System.EventHandler(this.btnConexion_Click);
             // 
             // panelSubMenuVentas
             // 
@@ -75,7 +86,7 @@ namespace Clave2_Grupo
             this.panelSubMenuVentas.Controls.Add(this.btnVenderTarjetas);
             this.panelSubMenuVentas.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelSubMenuVentas.Location = new System.Drawing.Point(0, 361);
-            this.panelSubMenuVentas.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panelSubMenuVentas.Margin = new System.Windows.Forms.Padding(4);
             this.panelSubMenuVentas.Name = "panelSubMenuVentas";
             this.panelSubMenuVentas.Size = new System.Drawing.Size(333, 154);
             this.panelSubMenuVentas.TabIndex = 6;
@@ -85,7 +96,7 @@ namespace Clave2_Grupo
             this.btnVenderJuegos.Dock = System.Windows.Forms.DockStyle.Top;
             this.btnVenderJuegos.FlatAppearance.BorderSize = 0;
             this.btnVenderJuegos.Location = new System.Drawing.Point(0, 43);
-            this.btnVenderJuegos.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnVenderJuegos.Margin = new System.Windows.Forms.Padding(4);
             this.btnVenderJuegos.Name = "btnVenderJuegos";
             this.btnVenderJuegos.Padding = new System.Windows.Forms.Padding(47, 0, 0, 0);
             this.btnVenderJuegos.Size = new System.Drawing.Size(333, 43);
@@ -100,7 +111,7 @@ namespace Clave2_Grupo
             this.btnVenderTarjetas.Dock = System.Windows.Forms.DockStyle.Top;
             this.btnVenderTarjetas.FlatAppearance.BorderSize = 0;
             this.btnVenderTarjetas.Location = new System.Drawing.Point(0, 0);
-            this.btnVenderTarjetas.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnVenderTarjetas.Margin = new System.Windows.Forms.Padding(4);
             this.btnVenderTarjetas.Name = "btnVenderTarjetas";
             this.btnVenderTarjetas.Padding = new System.Windows.Forms.Padding(47, 0, 0, 0);
             this.btnVenderTarjetas.Size = new System.Drawing.Size(333, 43);
@@ -119,7 +130,7 @@ namespace Clave2_Grupo
             this.btnMenuVentasAbrir.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnMenuVentasAbrir.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.btnMenuVentasAbrir.Location = new System.Drawing.Point(0, 314);
-            this.btnMenuVentasAbrir.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnMenuVentasAbrir.Margin = new System.Windows.Forms.Padding(4);
             this.btnMenuVentasAbrir.Name = "btnMenuVentasAbrir";
             this.btnMenuVentasAbrir.Size = new System.Drawing.Size(333, 47);
             this.btnMenuVentasAbrir.TabIndex = 5;
@@ -133,7 +144,7 @@ namespace Clave2_Grupo
             this.panelSubMenuTarjetas.BackColor = System.Drawing.Color.Teal;
             this.panelSubMenuTarjetas.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelSubMenuTarjetas.Location = new System.Drawing.Point(0, 154);
-            this.panelSubMenuTarjetas.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panelSubMenuTarjetas.Margin = new System.Windows.Forms.Padding(4);
             this.panelSubMenuTarjetas.Name = "panelSubMenuTarjetas";
             this.panelSubMenuTarjetas.Size = new System.Drawing.Size(333, 160);
             this.panelSubMenuTarjetas.TabIndex = 3;
@@ -145,7 +156,7 @@ namespace Clave2_Grupo
             this.panelSubMenuClientes.Controls.Add(this.btnNuevoCliente);
             this.panelSubMenuClientes.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelSubMenuClientes.Location = new System.Drawing.Point(0, 67);
-            this.panelSubMenuClientes.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panelSubMenuClientes.Margin = new System.Windows.Forms.Padding(4);
             this.panelSubMenuClientes.Name = "panelSubMenuClientes";
             this.panelSubMenuClientes.Size = new System.Drawing.Size(333, 87);
             this.panelSubMenuClientes.TabIndex = 1;
@@ -155,7 +166,7 @@ namespace Clave2_Grupo
             this.btnConsultarInfoClientes.Dock = System.Windows.Forms.DockStyle.Top;
             this.btnConsultarInfoClientes.FlatAppearance.BorderSize = 0;
             this.btnConsultarInfoClientes.Location = new System.Drawing.Point(0, 43);
-            this.btnConsultarInfoClientes.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnConsultarInfoClientes.Margin = new System.Windows.Forms.Padding(4);
             this.btnConsultarInfoClientes.Name = "btnConsultarInfoClientes";
             this.btnConsultarInfoClientes.Padding = new System.Windows.Forms.Padding(47, 0, 0, 0);
             this.btnConsultarInfoClientes.Size = new System.Drawing.Size(333, 42);
@@ -170,7 +181,7 @@ namespace Clave2_Grupo
             this.btnNuevoCliente.Dock = System.Windows.Forms.DockStyle.Top;
             this.btnNuevoCliente.FlatAppearance.BorderSize = 0;
             this.btnNuevoCliente.Location = new System.Drawing.Point(0, 0);
-            this.btnNuevoCliente.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnNuevoCliente.Margin = new System.Windows.Forms.Padding(4);
             this.btnNuevoCliente.Name = "btnNuevoCliente";
             this.btnNuevoCliente.Padding = new System.Windows.Forms.Padding(47, 0, 0, 0);
             this.btnNuevoCliente.Size = new System.Drawing.Size(333, 43);
@@ -188,7 +199,7 @@ namespace Clave2_Grupo
             this.btnMenuClientesAbrir.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnMenuClientesAbrir.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.btnMenuClientesAbrir.Location = new System.Drawing.Point(0, 12);
-            this.btnMenuClientesAbrir.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnMenuClientesAbrir.Margin = new System.Windows.Forms.Padding(4);
             this.btnMenuClientesAbrir.Name = "btnMenuClientesAbrir";
             this.btnMenuClientesAbrir.Size = new System.Drawing.Size(333, 55);
             this.btnMenuClientesAbrir.TabIndex = 0;
@@ -201,7 +212,7 @@ namespace Clave2_Grupo
             // 
             this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelLogo.Location = new System.Drawing.Point(0, 0);
-            this.panelLogo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panelLogo.Margin = new System.Windows.Forms.Padding(4);
             this.panelLogo.Name = "panelLogo";
             this.panelLogo.Size = new System.Drawing.Size(333, 12);
             this.panelLogo.TabIndex = 0;
@@ -212,7 +223,7 @@ namespace Clave2_Grupo
             this.panelAbajo.Controls.Add(this.btnSalir1);
             this.panelAbajo.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.panelAbajo.Location = new System.Drawing.Point(333, 515);
-            this.panelAbajo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panelAbajo.Margin = new System.Windows.Forms.Padding(4);
             this.panelAbajo.Name = "panelAbajo";
             this.panelAbajo.Size = new System.Drawing.Size(1080, 123);
             this.panelAbajo.TabIndex = 1;
@@ -220,7 +231,7 @@ namespace Clave2_Grupo
             // btnSalir1
             // 
             this.btnSalir1.Location = new System.Drawing.Point(747, 91);
-            this.btnSalir1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSalir1.Margin = new System.Windows.Forms.Padding(4);
             this.btnSalir1.Name = "btnSalir1";
             this.btnSalir1.Size = new System.Drawing.Size(100, 28);
             this.btnSalir1.TabIndex = 2;
@@ -233,20 +244,10 @@ namespace Clave2_Grupo
             this.panelContenedorDeFormularios.BackColor = System.Drawing.SystemColors.ControlDark;
             this.panelContenedorDeFormularios.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panelContenedorDeFormularios.Location = new System.Drawing.Point(333, 0);
-            this.panelContenedorDeFormularios.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panelContenedorDeFormularios.Margin = new System.Windows.Forms.Padding(4);
             this.panelContenedorDeFormularios.Name = "panelContenedorDeFormularios";
             this.panelContenedorDeFormularios.Size = new System.Drawing.Size(1080, 515);
             this.panelContenedorDeFormularios.TabIndex = 2;
-            // 
-            // btnConexion
-            // 
-            this.btnConexion.Location = new System.Drawing.Point(71, 570);
-            this.btnConexion.Name = "btnConexion";
-            this.btnConexion.Size = new System.Drawing.Size(175, 23);
-            this.btnConexion.TabIndex = 7;
-            this.btnConexion.Text = "Comprobar Conexion";
-            this.btnConexion.UseVisualStyleBackColor = true;
-            this.btnConexion.Click += new System.EventHandler(this.btnConexion_Click);
             // 
             // fmrPrincipal
             // 
@@ -256,7 +257,8 @@ namespace Clave2_Grupo
             this.Controls.Add(this.panelContenedorDeFormularios);
             this.Controls.Add(this.panelAbajo);
             this.Controls.Add(this.panelMenuMayorLateral);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "fmrPrincipal";
             this.Text = ":::GameSIVAR:::";
             this.panelMenuMayorLateral.ResumeLayout(false);
