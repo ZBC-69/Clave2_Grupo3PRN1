8d2d761d2b94aa8d37dc3f532f239bda29d3e752
Andrea Michelle Rodr�guez Hern�ndez <RH21053@ti.ues.edu.sv>
2023-11-08 22:44:59 -0600

    G

--- Clases/Conexion.cs
+++ Clases/Conexion.cs
@@ -13,7 +13,7 @@ namespace Clave2_Grupo.Clases
         MySqlConnection conexion = new MySqlConnection();
 
         static string servidor = "localhost";
-        static string bd = "Clave2_grupo3_";
+        static string bd = "Clave2_Grupo3_";
         static string usuario = "root";
         static string password = "root";
         static string puerto = "3306";
