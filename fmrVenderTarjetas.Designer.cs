d56db6485aeb67625d45f0e35c3b4db5c7299feb
Andrea Michelle Rodr�guez Hern�ndez <RH21053@ti.ues.edu.sv>
2023-11-10 16:18:02 -0600

    tarjetas desing

--- fmrVenderTarjetas.Designer.cs
+++ fmrVenderTarjetas.Designer.cs
@@ -30,6 +30,7 @@ namespace Clave2_Grupo
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fmrVenderTarjetas));
             this.btnSalir2 = new System.Windows.Forms.Button();
             this.btnContinuar = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
@@ -53,22 +54,30 @@ namespace Clave2_Grupo
             // 
             // btnSalir2
             // 
-            this.btnSalir2.Location = new System.Drawing.Point(284, 383);
+            this.btnSalir2.BackColor = System.Drawing.Color.DarkKhaki;
+            this.btnSalir2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSalir2.Location = new System.Drawing.Point(379, 471);
+            this.btnSalir2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnSalir2.Name = "btnSalir2";
-            this.btnSalir2.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir2.Size = new System.Drawing.Size(100, 28);
             this.btnSalir2.TabIndex = 2;
             this.btnSalir2.Text = "CERRAR";
-            this.btnSalir2.UseVisualStyleBackColor = true;
+            this.btnSalir2.UseVisualStyleBackColor = false;
             this.btnSalir2.Click += new System.EventHandler(this.btnSalir2_Click);
             // 
             // btnContinuar
             // 
-            this.btnContinuar.Location = new System.Drawing.Point(376, 214);
+            this.btnContinuar.BackColor = System.Drawing.Color.DarkKhaki;
+            this.btnContinuar.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnContinuar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnContinuar.Location = new System.Drawing.Point(501, 263);
+            this.btnContinuar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnContinuar.Name = "btnContinuar";
-            this.btnContinuar.Size = new System.Drawing.Size(75, 23);
+            this.btnContinuar.Size = new System.Drawing.Size(100, 28);
             this.btnContinuar.TabIndex = 3;
             this.btnContinuar.Text = "Continuar";
-            this.btnContinuar.UseVisualStyleBackColor = true;
+            this.btnContinuar.UseVisualStyleBackColor = false;
             this.btnContinuar.Click += new System.EventHandler(this.btnContinuar_Click);
             // 
             // groupBox1
@@ -79,9 +88,11 @@ namespace Clave2_Grupo
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(52, 34);
+            this.groupBox1.Location = new System.Drawing.Point(69, 42);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(263, 136);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(351, 167);
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Resumen de tarjetas disponibles";
@@ -89,72 +100,80 @@ namespace Clave2_Grupo
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(173, 105);
+            this.label6.Location = new System.Drawing.Point(231, 129);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(46, 13);
+            this.label6.Size = new System.Drawing.Size(60, 17);
             this.label6.TabIndex = 5;
             this.label6.Text = "$300.00";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(173, 34);
+            this.label5.Location = new System.Drawing.Point(231, 42);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.Size = new System.Drawing.Size(52, 17);
             this.label5.TabIndex = 4;
             this.label5.Text = "$50.00";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(173, 68);
+            this.label4.Location = new System.Drawing.Point(231, 84);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.Size = new System.Drawing.Size(60, 17);
             this.label4.TabIndex = 3;
             this.label4.Text = "$150.00";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(27, 105);
+            this.label3.Location = new System.Drawing.Point(36, 129);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.Size = new System.Drawing.Size(38, 17);
             this.label3.TabIndex = 2;
             this.label3.Text = "Gold";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(27, 68);
+            this.label2.Location = new System.Drawing.Point(36, 84);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.Size = new System.Drawing.Size(43, 17);
             this.label2.TabIndex = 1;
             this.label2.Text = "Silver";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(27, 34);
+            this.label1.Location = new System.Drawing.Point(36, 42);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.Size = new System.Drawing.Size(35, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "Plus";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(362, 78);
+            this.label7.Location = new System.Drawing.Point(483, 96);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(40, 13);
+            this.label7.Size = new System.Drawing.Size(53, 17);
             this.label7.TabIndex = 5;
             this.label7.Text = "Tarjeta";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(362, 139);
+            this.label8.Location = new System.Drawing.Point(483, 171);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(49, 13);
+            this.label8.Size = new System.Drawing.Size(64, 17);
             this.label8.TabIndex = 6;
             this.label8.Text = "Cantidad";
             // 
@@ -165,16 +184,18 @@ namespace Clave2_Grupo
             "Plus",
             "Silver",
             "Gold"});
-            this.cmbTarjetasComprar.Location = new System.Drawing.Point(435, 78);
+            this.cmbTarjetasComprar.Location = new System.Drawing.Point(580, 96);
+            this.cmbTarjetasComprar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cmbTarjetasComprar.Name = "cmbTarjetasComprar";
-            this.cmbTarjetasComprar.Size = new System.Drawing.Size(121, 21);
+            this.cmbTarjetasComprar.Size = new System.Drawing.Size(160, 24);
             this.cmbTarjetasComprar.TabIndex = 7;
             // 
             // txtNumTarjetasComprar
             // 
-            this.txtNumTarjetasComprar.Location = new System.Drawing.Point(435, 139);
+            this.txtNumTarjetasComprar.Location = new System.Drawing.Point(580, 171);
+            this.txtNumTarjetasComprar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtNumTarjetasComprar.Name = "txtNumTarjetasComprar";
-            this.txtNumTarjetasComprar.Size = new System.Drawing.Size(121, 20);
+            this.txtNumTarjetasComprar.Size = new System.Drawing.Size(160, 22);
             this.txtNumTarjetasComprar.TabIndex = 8;
             this.txtNumTarjetasComprar.Validating += new System.ComponentModel.CancelEventHandler(this.txtNumTarjetasComprar_Validating);
             // 
@@ -185,37 +206,47 @@ namespace Clave2_Grupo
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(403, 43);
+            this.label9.Location = new System.Drawing.Point(537, 53);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(174, 13);
+            this.label9.Size = new System.Drawing.Size(233, 17);
             this.label9.TabIndex = 9;
             this.label9.Text = "Elija un tipo de tarjeta y la cantidad:";
             // 
             // btnIrComprarJuegos
             // 
-            this.btnIrComprarJuegos.Location = new System.Drawing.Point(551, 383);
+            this.btnIrComprarJuegos.BackColor = System.Drawing.Color.DarkKhaki;
+            this.btnIrComprarJuegos.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIrComprarJuegos.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnIrComprarJuegos.Location = new System.Drawing.Point(735, 471);
+            this.btnIrComprarJuegos.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnIrComprarJuegos.Name = "btnIrComprarJuegos";
-            this.btnIrComprarJuegos.Size = new System.Drawing.Size(75, 23);
+            this.btnIrComprarJuegos.Size = new System.Drawing.Size(100, 28);
             this.btnIrComprarJuegos.TabIndex = 10;
             this.btnIrComprarJuegos.Text = "SIGUIENTE";
-            this.btnIrComprarJuegos.UseVisualStyleBackColor = true;
+            this.btnIrComprarJuegos.UseVisualStyleBackColor = false;
             this.btnIrComprarJuegos.Click += new System.EventHandler(this.btnIrComprarJuegos_Click);
             // 
             // btnRegistros
             // 
-            this.btnRegistros.Location = new System.Drawing.Point(25, 383);
+            this.btnRegistros.BackColor = System.Drawing.Color.DarkKhaki;
+            this.btnRegistros.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegistros.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnRegistros.Location = new System.Drawing.Point(33, 471);
+            this.btnRegistros.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnRegistros.Name = "btnRegistros";
-            this.btnRegistros.Size = new System.Drawing.Size(135, 23);
+            this.btnRegistros.Size = new System.Drawing.Size(180, 28);
             this.btnRegistros.TabIndex = 11;
             this.btnRegistros.Text = "Ver Registros DB";
-            this.btnRegistros.UseVisualStyleBackColor = true;
+            this.btnRegistros.UseVisualStyleBackColor = false;
             this.btnRegistros.Click += new System.EventHandler(this.btnRegistros_Click);
             // 
             // fmrVenderTarjetas
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(638, 418);
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(851, 514);
             this.Controls.Add(this.btnRegistros);
             this.Controls.Add(this.btnIrComprarJuegos);
             this.Controls.Add(this.label9);
@@ -226,6 +257,9 @@ namespace Clave2_Grupo
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.btnContinuar);
             this.Controls.Add(this.btnSalir2);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "fmrVenderTarjetas";
             this.Text = "FORMULARIO VENTA DE TARJETAS";
             this.Load += new System.EventHandler(this.fmrVenderTarjetas_Load);
